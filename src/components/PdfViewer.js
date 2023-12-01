64ba6ca0c4a00aa512dacab0b8ad529ed5a797da
writetofahim <writetofahimahammad@gmail.com>
2023-12-01 11:43:28 +0600

    document delete implemented

--- src/components/PdfViewer.js
+++ src/components/PdfViewer.js
@@ -1,4 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { toast } from "react-toastify";
+import { AuthContext } from "../context/AuthContexts";
 import axiosInstance from "../utils/axiosInstance";
 import Loader from "./loader/Loader";
 
@@ -6,7 +9,8 @@ const PdfViewer = ({ endPoint, heading }) => {
   const [selectedPdf, setSelectedPdf] = useState(null);
   const [pdfData, setPdfData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  // console.log(endPoint);
+  const [clickedElement, setClickedElement] = useState(null);
+  const { user } = useContext(AuthContext);
   // fetching data
   useEffect(() => {
     setIsLoading(true);
@@ -25,7 +29,17 @@ const PdfViewer = ({ endPoint, heading }) => {
   const handlePdfClick = (pdf) => {
     setSelectedPdf(pdf);
   };
-
+  const handleDelete = async (id) => {
+    try {
+      await axiosInstance.delete(`documents/${id}`);
+      toast.success("delete success");
+    } catch (error) {
+      toast.error("delete fail!");
+    }
+  };
+  const isUrlAdmin = (url) => {
+    return url.includes("admin");
+  };
   return (
     <>
       {isLoading ? (
@@ -48,9 +62,44 @@ const PdfViewer = ({ endPoint, heading }) => {
                       className={`cursor-pointer  bg-white dark:bg-gray-500 shadow-md rounded-md px-2 mr-2 py-1 ${
                         selectedPdf === pdf ? "text-blue-600 font-semibold" : ""
                       }`}
-                      onClick={() => handlePdfClick(pdf)}
+                      onClick={(e) => {
+                        handlePdfClick(pdf);
+                        e.stopPropagation();
+                      }}
                     >
-                      {pdf.pdfTitle}
+                      {/* options */}
+                      <div
+                        className={`z-40 flex justify-end relative ${
+                          user?.success & isUrlAdmin(window.location.pathname)
+                            ? "block"
+                            : "hidden"
+                        }`}
+                      >
+                        <BsThreeDotsVertical
+                          className="text-xl cursor-pointer"
+                          onClick={(e) => {
+                            setClickedElement((prevProfile) =>
+                              prevProfile === null ? pdf : null
+                            );
+                            e.stopPropagation();
+                          }}
+                        />
+                        <div
+                          className={`absolute top-5 bg-white px-2 py-3 shadow ${
+                            clickedElement?._id === pdf?._id
+                              ? "block"
+                              : "hidden"
+                          }`}
+                        >
+                          <button
+                            onClick={() => handleDelete(pdf?._id)}
+                            className="block  p-2"
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                      <div>{pdf.pdfTitle}</div>
                     </li>
                   ))}
               </ul>
