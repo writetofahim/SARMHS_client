076d41b445a753e7e43f4769c2f41641e5ae05ab
fahim <fahim@thewiseminds.nl>
2024-03-22 19:44:10 +0600

    condition changed in test page

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -38,9 +38,9 @@ const Test = () => {
     </div>
     <div className="flex justify-center items-center mt-10 space-x-5">
       {
-        process.env.NODE_ENV === "development"?
-        <button className="border px-3 py-1 rounded-md ">Dev env</button> :
-        <button className="border px-3 py-1 rounded-md ">production</button>
+        process.env.NODE_ENV === "production"?
+        <button className="border px-3 py-1 rounded-md ">production</button> :
+        <button className="border px-3 py-1 rounded-md ">Dev env</button> 
       }
     
     </div>
